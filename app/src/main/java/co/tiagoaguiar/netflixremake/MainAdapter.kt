238cc19bb67ef57e00d8107886b256c1f24f5a60
André Morientes <59062594+andremorientes@users.noreply.github.com>
2023-09-25 17:22:42 +0200

    Separando o Adapter da Main
      override fun onCreateViewHolder(parent:
      //FICA ASSIM;
      ViewGroup, viewType: Int): MovieViewHolder {
            val view = LayoutInflater.from(parent.context).inflate(R.layout.move_item, parent, false)
    
            return MovieViewHolder(view )
        }

---
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainAdapter.kt
@@ -0,0 +1,32 @@
+package co.tiagoaguiar.netflixremake
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+ class MainAdapter : RecyclerView.Adapter<MainAdapter.MovieViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.move_item, parent, false)
+
+        return MovieViewHolder(view )
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return 60
+    }
+
+     class MovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+
+
+
+}
