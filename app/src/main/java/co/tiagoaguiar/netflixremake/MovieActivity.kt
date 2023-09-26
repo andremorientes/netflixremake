2dca279a7e0ff549512bc3eef5cda16afdf0d4ad
André Morientes <59062594+andremorientes@users.noreply.github.com>
2023-09-27 00:35:18 +0200

    Começando a desenhar a parte de detalhes ELEGANCIA

--- app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -18,9 +18,16 @@ class MovieActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        //Busquei o desenhavel (Layer-List)
         val layerDrawable : LayerDrawable = ContextCompat.getDrawable(this,R.drawable.shadows) as LayerDrawable
+
+        //Busquei o filme que eu quero
         val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
+
+        //Atribui a esse layer-list o novo filme
         layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+
+        //Set no ImageView
         val coverImg: ImageView= findViewById(R.id.move_img)
         coverImg.setImageDrawable(layerDrawable)
 
