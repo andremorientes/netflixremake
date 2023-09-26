fe7de2d6a83b16338d2e6f211d3d7f5ed9671ad9
André Morientes <59062594+andremorientes@users.noreply.github.com>
2023-09-27 00:21:44 +0200

    Desenhando sobreamento (SHADOWN) INSTANCIAR para poder alterar de forma dinamica ....

--- app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -1,8 +1,11 @@
 package co.tiagoaguiar.netflixremake
 
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +17,12 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val layerDrawable : LayerDrawable = ContextCompat.getDrawable(this,R.drawable.shadows) as LayerDrawable
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+        val coverImg: ImageView= findViewById(R.id.move_img)
+        coverImg.setImageDrawable(layerDrawable)
 
     }
 }
